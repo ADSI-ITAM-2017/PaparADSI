12dda497dbc044b69914a8cab4b60a4144d0af6d
ximenacn <ximena.carrillo.nor@gmail.com>
2017-05-18 02:22:35 -0500

    Back-End

--- rumi/db/migrate/20170501043022_create_publicars.rb
+++ rumi/db/migrate/20170501043022_create_publicars.rb
@@ -1,6 +1,9 @@
 class CreatePublicars < ActiveRecord::Migration[5.0]
   def change
     create_table :publicars do |t|
+      
+      t.string :titulo
+      t.integer :Precio
       t.text :description
 
       t.timestamps
