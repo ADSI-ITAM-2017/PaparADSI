898f78dee0263358c27d6ba5c847ea8a304f5f03
JuanCAguilar <juanc.aguilar94@gmail.com>
2017-05-17 23:47:15 -0500

    avances pagina

--- rumi/db/schema.rb
+++ rumi/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517023533) do
+ActiveRecord::Schema.define(version: 20170517045221) do
 
   create_table "chats", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversas", force: :cascade do |t|
+    t.integer  "recipient_id"
+    t.integer  "sender_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["recipient_id"], name: "index_conversas_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversas_on_sender_id"
   end
 
   create_table "perfils", force: :cascade do |t|
